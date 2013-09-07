8b98a0f51e73db9b3035e11b5887da71f5507504
qnhoang81 <qnhoang81@gmail.com>
2013-09-07 11:04:31 -0400

    Redo layout. Add Nav drawer. Change some icons. Add search function.

--- src/com/online/GCM/activities/GcmOnlineActivity.java
+++ src/com/online/GCM/activities/GcmOnlineActivity.java
@@ -3,7 +3,12 @@ package com.online.GCM.activities;
 import android.app.Activity;
 import android.app.Fragment;
 import android.app.FragmentManager;
+import android.content.Intent;
+import android.content.res.Configuration;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.SystemClock;
+import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
@@ -21,48 +26,147 @@ import com.online.GCM.fragments.SermonsFragment;
  */
 public class GcmOnlineActivity extends Activity {
 
-    private static WebView mWebView;
+    private WebView mWebViewGcm;
 
-    public static void updateWebView() {
+    public void updateWebView() {
 
-        mWebView.setWebViewClient(new WebViewClient());
-        WebSettings webSettings = mWebView.getSettings();
+        WebSettings webSettings = mWebViewGcm.getSettings();
+        CookieManager cookieManager = CookieManager.getInstance();
 
         webSettings.setJavaScriptEnabled(true);
-        mWebView.getSettings().setSupportZoom(true);
-        mWebView.getSettings().setBuiltInZoomControls(true);
-        mWebView.setScrollBarStyle(WebView.SCROLLBARS_OUTSIDE_OVERLAY);
-        mWebView.setScrollbarFadingEnabled(true);
-        mWebView.getSettings().setLoadsImagesAutomatically(true);
+        webSettings.setSupportZoom(true);
+        webSettings.setBuiltInZoomControls(true);
+        webSettings.setLoadsImagesAutomatically(true);
+        webSettings.setLoadWithOverviewMode(true);
+        webSettings.setUseWideViewPort(true);
+
+        mWebViewGcm.setWebViewClient(new WebViewClient() {
+
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                view.loadUrl(url);
+                return true;
+            }
+
+            @Override
+            public void onPageFinished(WebView view, String url) {
+
+                CookieSyncManager.getInstance().sync();
+                // Get the cookie from cookie jar.
+                String cookie = CookieManager.getInstance().getCookie(url);
+                if (cookie == null) {
+                    return;
+                }
+                // Cookie is a string like NAME=VALUE [; NAME=VALUE]
+                String[] pairs = cookie.split(";");
+                for (int i = 0; i < pairs.length; ++i) {
+                    String[] parts = pairs[i].split("=", 2);
+                    // If token is found, return it to the calling activity.
+                    if (parts.length == 2 &&
+                            parts[0].equalsIgnoreCase("oauth_token")) {
+                        Intent result = new Intent();
+                        result.putExtra("token", parts[1]);
+                        setResult(RESULT_OK, result);
+                        finish();
+                    }
+                }
+            }
+        });
+
+        mWebViewGcm.loadUrl("file:///android_asset/header.html");
+        mWebViewGcm.setScrollBarStyle(WebView.SCROLLBARS_OUTSIDE_OVERLAY);
+        mWebViewGcm.setScrollbarFadingEnabled(true);
+
+
+        cookieManager.setAcceptCookie(true);
+
+    }
+
+    public void homescreen() {
+        Intent homeIntent= new Intent(Intent.ACTION_MAIN);
+        homeIntent.addCategory(Intent.CATEGORY_HOME);
+        homeIntent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        startActivity(homeIntent);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        CookieSyncManager.getInstance().stopSync();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        CookieSyncManager.getInstance().startSync();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        switch (requestCode) {
+            case 0:
+                if (resultCode != RESULT_OK || data == null) {
+                    return;
+                }
+                // Get the token.
+                String token = data.getStringExtra("token");
+                if (token != null) {
+                /* Use the token to access data */
+                }
+                return;
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        // Check if the key event was the Back button and if there's history
+        if ((keyCode == KeyEvent.KEYCODE_BACK) && mWebViewGcm.canGoBack()) {
+            mWebViewGcm.goBack();
+            return true;
+        }  else if (keyCode == KeyEvent.KEYCODE_BACK) {
+            homescreen();
+            return true;
+        }
+
+        // If it wasn't the Back key or there's no web page history, bubble up to the default
+        // system behavior (probably exit the activity)
+        return super.onKeyDown(keyCode, event);
+
     }
 
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.gcm_online);
 
-        mWebView = (WebView) findViewById(R.id.webviewgcm);
-        mWebView.loadUrl("https://gracechurchmentor.ccbchurch.com/login.php");
+        mWebViewGcm = (WebView) findViewById(R.id.webviewgcm);
+        //mWebViewGcm.loadUrl("https://gracechurchmentor.ccbchurch.com/login.php");
 
+        new WebViewTask().execute();
         updateWebView();
 
         if (savedInstanceState == null) {
             // Load a page
-            mWebView.loadUrl("https://gracechurchmentor.ccbchurch.com/login.php");
+            //mWebViewGcm.loadUrl("https://gracechurchmentor.ccbchurch.com/login.php");
         }
 
+        CookieSyncManager.createInstance(getApplicationContext());
+
+
     }
+
     @Override
     protected void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState);
         // Save the state of the WebView
-        mWebView.saveState(outState);
+        mWebViewGcm.saveState(outState);
     }
 
     @Override
     protected void onRestoreInstanceState(Bundle savedInstanceState) {
         super.onRestoreInstanceState(savedInstanceState);
         // Restore the state of the WebView
-        mWebView.restoreState(savedInstanceState);
+        mWebViewGcm.restoreState(savedInstanceState);
     }
 
     @Override
@@ -72,26 +176,36 @@ public class GcmOnlineActivity extends Activity {
         return true;
     }
 
-    @Override
+    /*@Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        FragmentManager manager = getFragmentManager();
-        android.app.FragmentTransaction transaction = manager.beginTransaction();
-        Fragment frag = null;
-        // Handle item selection
+
         switch (item.getItemId()) {
+            case R.id.home:
+                mWebViewGcm.loadUrl("http://www.gracechurchmentor.org");
+                break;
             case R.id.calendar:
-                frag = new CalendarFragment();
+                mWebViewGcm.loadUrl("http://www.gracechurchmentor.org/calendar");
                 break;
             case R.id.Sermons:
-                frag = new SermonsFragment();
+                mWebViewGcm.loadUrl("http://www.gracechurchmentor.org/sermons");
+                break;
+            case R.id.exit:
+                homescreen();
                 break;
             default:
-                frag = new CalendarFragment();
+                mWebViewGcm.loadUrl("https://gracechurchmentor.ccbchurch.com/login.php");
         }
 
-        transaction.replace(R.id.webviewhome, frag);
-        transaction.commit();
-
         return super.onOptionsItemSelected(item);
+    }    */
+
+    private class WebViewTask extends AsyncTask<Void, Void, Boolean> {
+
+        protected Boolean doInBackground(Void... param) {
+            /* this is very important - THIS IS THE HACK */
+            SystemClock.sleep(1000);
+            return false;
+        }
     }
+
 }
--- src/com/online/GCM/activities/HomeActivity.java
+++ src/com/online/GCM/activities/HomeActivity.java
@@ -1,22 +1,31 @@
 package com.online.GCM.activities;
 
-import android.app.ActionBar;
 import android.app.Activity;
-import android.app.Fragment;
-import android.app.FragmentManager;
-import android.nfc.Tag;
+import android.app.SearchManager;
+import android.content.Intent;
+import android.content.res.Configuration;
+import android.graphics.Color;
+import android.os.AsyncTask;
 import android.os.Bundle;
-import android.support.v4.app.FragmentActivity;
-import android.util.Log;
-import android.view.KeyEvent;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
-import android.webkit.*;
+import android.os.SystemClock;
+import android.support.v4.app.ActionBarDrawerToggle;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.view.*;
+import android.webkit.CookieManager;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 import com.example.GCM.R;
-import com.online.GCM.fragments.CalendarFragment;
-import com.online.GCM.fragments.HomeFragment;
-import com.online.GCM.fragments.SermonsFragment;
+import org.apache.commons.io.IOUtils;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.net.URLConnection;
 
 /**
  * Created with IntelliJ IDEA.
@@ -27,96 +36,254 @@ import com.online.GCM.fragments.SermonsFragment;
  */
 public class HomeActivity extends Activity {
 
-    private static WebView mWebView;
+    private WebView mWebViewhome;
+    private DrawerLayout mDrawerLayout;
+    private ListView mDrawerList;
+    private ActionBarDrawerToggle mDrawerToggle;
+    private CharSequence mDrawerTitle;
+    private CharSequence mTitle;
+    private String[] mNavigate;
 
-    private static final String HOME_STATE = "homestate";
+    public void updateWebView() {
 
-    public static void updateWebView() {
+        WebSettings mWebSettings = mWebViewhome.getSettings();
+        CookieManager cookieManager = CookieManager.getInstance();
 
-        mWebView.setWebViewClient(new WebViewClient());
-        WebSettings webSettings = mWebView.getSettings();
+        mWebSettings.setJavaScriptEnabled(true);
+        mWebSettings.setSupportZoom(true);
+        mWebSettings.setBuiltInZoomControls(true);
+        mWebSettings.setLoadsImagesAutomatically(true);
 
-        webSettings.setJavaScriptEnabled(true);
-        mWebView.getSettings().setSupportZoom(true);
-        mWebView.getSettings().setBuiltInZoomControls(true);
-        mWebView.setScrollBarStyle(WebView.SCROLLBARS_OUTSIDE_OVERLAY);
-        mWebView.setScrollbarFadingEnabled(true);
-        mWebView.getSettings().setLoadsImagesAutomatically(true);
+        mWebViewhome.setScrollBarStyle(WebView.SCROLLBARS_OUTSIDE_OVERLAY);
+        mWebViewhome.setScrollbarFadingEnabled(true);
+
+        //mWebViewhome.loadUrl("file:///android_asset/gracechurchmentor.html");
+        cookieManager.setAcceptCookie(true);
     }
+
+    public static String getURL() throws IOException {
+
+        URL url = new URL("http://www.gracechurchmentor.org");
+        URLConnection con = url.openConnection();
+        InputStream in = con.getInputStream();
+        String encoding = con.getContentEncoding();
+        encoding = encoding == null ? "UTF-8" : encoding;
+        String str = IOUtils.toString(in, encoding);
+
+        return str;
+
+    }
+
 
     @Override
     public boolean onKeyDown(int keyCode, KeyEvent event) {
         // Check if the key event was the Back button and if there's history
-        if ((keyCode == KeyEvent.KEYCODE_BACK) && mWebView.canGoBack()) {
-            mWebView.goBack();
+        if ((keyCode == KeyEvent.KEYCODE_BACK) && mWebViewhome.canGoBack()) {
+            mWebViewhome.goBack();
+            return true;
+        } else if (keyCode == KeyEvent.KEYCODE_BACK) {
+            homescreen();
             return true;
         }
         // If it wasn't the Back key or there's no web page history, bubble up to the default
         // system behavior (probably exit the activity)
+
         return super.onKeyDown(keyCode, event);
+    }
+
+    public void homescreen() {
+        Intent homeIntent= new Intent(Intent.ACTION_MAIN);
+        homeIntent.addCategory(Intent.CATEGORY_HOME);
+        homeIntent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        startActivity(homeIntent);
     }
 
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_ACTION_BAR);
         setContentView(R.layout.home);
 
-        mWebView = (WebView) findViewById(R.id.webviewhome);
-        mWebView.loadUrl("http://www.gracechurchmentor.org");
+        /* Webview implementation */
 
+        mWebViewhome = (WebView) findViewById(R.id.webviewhome);
         updateWebView();
 
-        if (savedInstanceState == null) {
-            // Load a page
-            mWebView.loadUrl("http://www.gracechurchmentor.org");
-        }
+        mWebViewhome.setWebViewClient(new WebViewClient() {
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView webView, String url) {
+                return false;
+            }
+
+        });
+
+            try {
+                InputStream st = getAssets().open("header.html");
+                InputStream st2 = getAssets().open("footer.html");
+                String header = IOUtils.toString(st);
+                String footer = IOUtils.toString(st2);
+
+                String html = getURL().substring(getURL().indexOf("<div id=\"k2ModuleBox127\" "), getURL().indexOf("Admit, Believe, Commit"));
+
+                StringBuilder builder = new StringBuilder("");
+                builder.append(header + html + footer + "</div></div></body></html>");
+                mWebViewhome.loadDataWithBaseURL(null, builder.toString(), "text/html", "UTF-8", null);
+
+            } catch (IOException e1) {
+                e1.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+            }
+
+            new WebViewTask().execute();
+
+            if (savedInstanceState == null) {
+                // Load a page
+               // mWebViewhome.loadUrl("file:///android_asset/gracechurchmentor.html");
+            }
+
+        getActionBar().setDisplayHomeAsUpEnabled(true);
+        getActionBar().setHomeButtonEnabled(true);
+
+        /*Implemenation of Sliding Navigation Drawer*/
+
+        mNavigate = getResources().getStringArray(R.array.navigate_array);
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        mDrawerList = (ListView) findViewById(R.id.left_drawer);
+
+        // set a custom shadow that overlays the main content when the drawer opens
+        mDrawerLayout.setDrawerShadow(R.drawable.drawer_shadow, GravityCompat.START);
+        mDrawerLayout.setBackgroundColor(R.color.transparent);
+        // set up the drawer's list view with items and click listener
+        mDrawerList.setAdapter(new ArrayAdapter<String>(this,
+                R.layout.drawer_list_item, mNavigate));
+        //mDrawerList.setCacheColorHint(Color.TRANSPARENT);
+        mDrawerList.setBackgroundColor(R.color.transparent);
+        //mDrawerList.setOnItemClickListener(new DrawerItemClickListener());
+
+        mTitle = mDrawerTitle = getTitle();
+        mDrawerToggle = new ActionBarDrawerToggle(
+                this,                  /* host Activity */
+                mDrawerLayout,         /* DrawerLayout object */
+                R.drawable.ic_drawer,  /* nav drawer image to replace 'Up' caret */
+                R.string.drawer_open,  /* "open drawer" description for accessibility */
+                R.string.drawer_close  /* "close drawer" description for accessibility */
+        ) {
+            public void onDrawerClosed(View view) {
+                getActionBar().setTitle(mTitle);
+                invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
+            }
+
+            public void onDrawerOpened(View drawerView) {
+                getActionBar().setTitle(mDrawerTitle);
+                invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
+            }
+        };
+        mDrawerLayout.setDrawerListener(mDrawerToggle);
 
     }
+
     @Override
     protected void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState);
         // Save the state of the WebView
-        mWebView.saveState(outState);
+        mWebViewhome.saveState(outState);
     }
 
     @Override
     protected void onRestoreInstanceState(Bundle savedInstanceState) {
         super.onRestoreInstanceState(savedInstanceState);
         // Restore the state of the WebView
-        mWebView.restoreState(savedInstanceState);
+        mWebViewhome.restoreState(savedInstanceState);
+    }
+
+
+    private class WebViewTask extends AsyncTask<Void, Void, Boolean> {
+
+        protected Boolean doInBackground(Void... param) {
+            /* this is very important - THIS IS THE HACK */
+            SystemClock.sleep(1000);
+            return false;
+        }
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.navigate, menu);
-        return true;
+        return super.onCreateOptionsMenu(menu);
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        FragmentManager manager = getFragmentManager();
-        android.app.FragmentTransaction transaction = manager.beginTransaction();
-        Fragment frag = null;
-        // Handle item selection
-        switch (item.getItemId()) {
-            case R.id.home:
-                frag = new HomeFragment();
-                break;
-            case R.id.calendar:
-                frag = new CalendarFragment();
-                break;
-            case R.id.Sermons:
-                frag = new SermonsFragment();
-                break;
-            default:
-                frag = new HomeFragment();
+
+        // The action bar home/up action should open or close the drawer.
+        // ActionBarDrawerToggle will take care of this.
+        if (mDrawerToggle.onOptionsItemSelected(item)) {
+            return true;
         }
-
-        transaction.replace(R.id.webviewhome, frag);
-        transaction.commit();
-
-        return super.onOptionsItemSelected(item);
+        // Handle action buttons
+        switch(item.getItemId()) {
+            case R.id.action_websearch:
+                // create intent to perform web search for this planet
+                Intent intent = new Intent(Intent.ACTION_WEB_SEARCH);
+                intent.putExtra(SearchManager.QUERY, getActionBar().getTitle());
+                // catch event that there's no activity to handle intent
+                if (intent.resolveActivity(getPackageManager()) != null) {
+                    startActivity(intent);
+                } else {
+                    Toast.makeText(this, R.string.app_not_available, Toast.LENGTH_LONG).show();
+                }
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 
+        /*private class DrawerItemClickListener implements ListView.OnItemClickListener {
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+            selectItem(position);
+        }
+    }*/
+
+    /*private void selectItem(int position) {
+        // update the main content by replacing fragments
+        Fragment fragment = new PlanetFragment();
+        Bundle args = new Bundle();
+        args.putInt(PlanetFragment.ARG_PLANET_NUMBER, position);
+        fragment.setArguments(args);
+
+        FragmentManager fragmentManager = getFragmentManager();
+        fragmentManager.beginTransaction().replace(R.id.content_frame, fragment).commit();
+
+        // update selected item and title, then close the drawer
+        mDrawerList.setItemChecked(position, true);
+        setTitle(mPlanetTitles[position]);
+        mDrawerLayout.closeDrawer(mDrawerList);
+    } */
+
+
+    @Override
+    public void setTitle(CharSequence title) {
+        mTitle = title;
+        getActionBar().setTitle(mTitle);
+    }
+
+    /**
+     * When using the ActionBarDrawerToggle, you must call it during
+     * onPostCreate() and onConfigurationChanged()...
+     */
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        // Sync the toggle state after onRestoreInstanceState has occurred.
+        mDrawerToggle.syncState();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        // Pass any configuration change to the drawer toggls
+        mDrawerToggle.onConfigurationChanged(newConfig);
+    }
 
 }
